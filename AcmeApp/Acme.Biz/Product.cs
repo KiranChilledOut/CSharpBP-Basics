671de7c7e2ffe32a3d20539abff7f2d65eed2a35
KiranChilledOut <citadelforsomerefuge@gmail.com>
2018-02-27 01:06:03 +0530

    1

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// This class is used for products in the biz
+    /// </summary>
+    public class Product
+    {
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private int productID;
+
+        public int ProductID
+        {
+            get { return productID; }
+            set { productID = value; }
+        }
+
+        public string SayHello()
+        {
+            return $"Hello {ProductName} ({ProductID}): {Description}";
+        }
+    }
+}
---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var productTest = new Product();
+            productTest.ProductName = "Zoom10";
+            productTest.ProductID = 1;
+            productTest.Description = "This is a Nike 10 Size Shoes,Model Zoom";
+
+            //Act
+            string expected = "Hello Zoom10 (1): This is a Nike 10 Size Shoes,Model Zoom";
+            var actual=productTest.SayHello();
+            //Assert
+            Assert.AreEqual(actual,expected);
+        }
+    }
+}
